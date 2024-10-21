f826c470add0b529166b82b79ab751e83f98b098
ChengyanTsai-TAMU <maxwellcai613@gmail.com>
2024-10-20 22:07:06 -0500

    Updated Arts Page

---
+++ assets/js/arts.js
@@ -0,0 +1,32 @@
+
+let slideIndexes = {
+  "2D": 1,
+  "pixel": 1,
+  "vfx": 1
+};
+
+function showSlides(n, type) {
+  let slides = document.getElementsByClassName("slide-" + type);
+  if (n > slides.length) {
+    slideIndexes[type] = 1;
+  }
+  if (n < 1) {
+    slideIndexes[type] = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndexes[type] - 1].style.display = "block";
+}
+
+function moveSlides(n, type) {
+  showSlides(slideIndexes[type] += n, type);
+}
+
+// Initialize sliders
+document.addEventListener("DOMContentLoaded", function() {
+  showSlides(1, "2D");
+  showSlides(1, "pixel");
+  showSlides(1, "vfx");
+});
+
